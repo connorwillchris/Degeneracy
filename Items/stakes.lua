53bcdf85b2b9d168404dc5a5cb5159df2c5a4789
Gud <toby.hayter10@gmail.com>
2025-12-24 18:59:35 +0000

    Refactor calculate function in stakes.lua

--- Items/stakes.lua
+++ Items/stakes.lua
@@ -16,11 +16,18 @@ SMODS.Stake({
     prefix_config = { above_stake = { false }, applied_stakes = { mod = false } },
     colour = HEX("d2ad64"),
     shiny = true,
-
-    calculate = function (self, context)
+    calculate = function(self, context)
+        if G.GAME.modifiers.enable_deg_locked and not G.GAME.brass_applied then
+            G.GAME.brass_applied = true -- so it only does it once
+            for i = 1, #G.playing_cards do
+                if SMODS.pseudorandom_probability(self, 'deg_seed', 1, 7, 'brass') then
+                    SMODS.Stickers["deg_locked"]:apply(G.playing_cards[i], true)
+                end
+            end
+        end
     end,
-
     modifiers = function()
         G.GAME.modifiers.enable_deg_locked = true
     end,
 })
+
