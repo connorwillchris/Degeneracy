8d48f9c05cc818ecba0a254579b24e05e77f51f4
cloudzXIII <100033104+cloudzXIII@users.noreply.github.com>
2026-01-27 17:05:35 +0000

    Odd todd and Even Steven (fix?)

--- Items/changes.lua
+++ Items/changes.lua
@@ -25,7 +25,6 @@ SMODS.Joker:take_ownership('fibonacci', {
         end
     end
 })
---[[ broken, will fix later
 SMODS.Joker:take_ownership('odd_todd', {
     config = { extra = { chips = 31 } },
     loc_vars = function(self, info_queue, card)
@@ -35,7 +34,7 @@ SMODS.Joker:take_ownership('odd_todd', {
     end,
     calculate = function(self, card, context)
         if context.individual and context.cardarea == G.play then
-            if not context.other_card:is_face() and (context.other_card:get_id() % 2 == 1 or context.other_card.base.nominal % 2 == 1 or context.other_card:get_id() == 14) then
+            if not context.other_card:is_face() and (context.other_card.base.nominal % 2 == 1 or context.other_card:get_id() == 14) then
                 return {
                     chips = card.ability.extra.chips
                 }
@@ -52,7 +51,7 @@ SMODS.Joker:take_ownership('even_steven', {
     end,
     calculate = function(self, card, context)
         if context.individual and context.cardarea == G.play then
-            if not context.other_card:is_face() and (context.other_card:get_id() % 2 == 0 or context.other_card.base.nominal % 2 == 0) then
+            if not context.other_card:is_face() and (context.other_card.base.nominal % 2 == 0) then
                 return {
                     mult = card.ability.extra.mult
                 }
@@ -60,12 +59,3 @@ SMODS.Joker:take_ownership('even_steven', {
         end
     end
 })
---]]
--- Name change, otherwise functionally the same
-SMODS.Joker:take_ownership('business', {
-    loc_vars = function(self, info_queue, card)
-        return {
-            key = 'j_deg_business',
-        }
-    end,
-}, true)
