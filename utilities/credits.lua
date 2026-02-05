1886478cd34194faabcc09b89a1ac1b3df9e62ed
cloudzXIII <100033104+cloudzXIII@users.noreply.github.com>
2026-02-05 21:47:23 +0000

    Credits Overhaul!
    *again*, added the ability to have multiple rows of cards

--- utilities/credits.lua
+++ utilities/credits.lua
@@ -6,10 +6,8 @@ DEG.Credits_Joker = SMODS.Joker:extend({
     loc_vars = function(self, info_queue, card)
         return { vars = {} }
     end,
-    set_card_type_badge = function(self, card, badges)
-    end,
 
-    atlas = 'deg_jokers',
+    atlas = 'deg_jokers', -- set atlas here
     pos = { x = 0, y = 0 },
     unlocked = true,
     discovered = true,
@@ -22,28 +20,63 @@ DEG.Credits_Joker = SMODS.Joker:extend({
     in_pool = function(self)
         return false
     end,
+    set_card_type_badge = function(self, card, badges)
+        if self.programmer then
+            badges[#badges + 1] = create_badge(localize('k_programmer_badge'), G.C.BLUE, G.C.WHITE, 1.2)
+        elseif self.artist then
+            badges[#badges + 1] = create_badge(localize('k_artist_badge'), G.C.PURPLE, G.C.WHITE, 1.2)
+        elseif self.ideas then
+            badges[#badges + 1] = create_badge("Ideas", G.C.GREEN, G.C.WHITE, 1.2)
+        end
+    end,
 })
 
+-- New Thing! can now just say programmer = true, artist = true, ideas = true etc. to add badge
 DEG.Credits_Joker {
     key = "cloudzXIII",
-    atlas = 'deg_jokers',
     pos = { x = 0, y = 0 },
-    set_card_type_badge = function(self, card, badges)
-        badges[#badges + 1] = create_badge(localize('k_programmer_badge'), G.C.BLUE, G.C.WHITE, 1.2)
-    end,
+    programmer = true,
 }
 DEG.Credits_Joker {
     key = "Gud",
-    atlas = 'deg_jokers',
     pos = { x = 0, y = 0 },
-    set_card_type_badge = function(self, card, badges)
-        badges[#badges + 1] = create_badge(localize('k_artist_badge'), G.C.RARITY.LEGENDARY, G.C.WHITE, 1.2)
-    end,
+    artist = true
 }
 
 SMODS.current_mod.credits_tab = function()
-    local loc_nodes = {}
+    local create_credits_row = function(cards)
+        local area = CardArea(
+            G.ROOM.T.x + 0.2 * G.ROOM.T.w / 2, G.ROOM.T.h,
+            4.25 * G.CARD_W,
+            0.95 * G.CARD_H,
+            { card_limit = #cards, type = 'title', highlight_limit = 0, collection = true }
+        )
 
+        for _, key in ipairs(cards) do
+            local card = Card(
+                area.T.x + area.T.w / 2,
+                area.T.y,
+                G.CARD_W,
+                G.CARD_H,
+                G.P_CARDS.empty,
+                G.P_CENTERS[key]
+            )
+            area:emplace(card)
+            card:juice_up(0.3)
+        end
+
+        return area
+    end
+
+    local credits_ppl = {
+        { ppl = { "j_joker", "j_deg_cloudzXIII", "j_deg_Gud", "j_joker", "j_joker" } },
+        -- (If you want another row uncomment below)
+        --{ ppl = { "j_joker", "j_deg_Gud", "j_joker" } },
+        { ppl = { "j_joker", "j_joker", "j_joker", "j_joker", "j_joker", "j_joker" } }
+    }
+
+    --#region Bit with the localised text, can remove/change if unneccessary
+    local loc_nodes = {}
     localize {
         type = "descriptions",
         set = "Other",
@@ -51,24 +84,34 @@ SMODS.current_mod.credits_tab = function()
         nodes = loc_nodes,
         vars = {}
     }
+    local row_nodes = {
+        -- commented localised text for now
+        --[[{
+            n = G.UIT.R,
+            config = {
+                align = "tm",
+                padding = 0,
+                minw = 8,
+                minh = 3
+            },
+            nodes = {
+                desc_from_rows(loc_nodes, true, 7)
+            }
+        }--]]
+    }
+    --#endregion
 
-    G.deg_credits_area = CardArea(
-        G.ROOM.T.x + 0.2 * G.ROOM.T.w / 2, G.ROOM.T.h,
-        4.25 * G.CARD_W,
-        0.95 * G.CARD_H,
-        { card_limit = 5, type = 'title', highlight_limit = 0, collection = true }
-    )
+    -- Insert ppl into row_nodes
+    for _, row in ipairs(credits_ppl) do
+        local area = create_credits_row(row.ppl)
 
-    -- add joker keys to credits_ppl to add more people
-    local credits_ppl = { "j_deg_Gud", "j_deg_cloudzXIII" }
-    for i, key in ipairs(credits_ppl) do
-        local card = Card(
-            G.deg_credits_area.T.x + G.deg_credits_area.T.w / 2, G.deg_credits_area.T.y,
-            G.CARD_W, G.CARD_H, G.P_CARDS.empty,
-            G.P_CENTERS[key])
-
-        G.deg_credits_area:emplace(card)
-        card:juice_up()
+        row_nodes[#row_nodes + 1] = {
+            n = G.UIT.R,
+            config = { align = "cm", padding = 0.07, no_fill = true },
+            nodes = {
+                { n = G.UIT.O, config = { object = area } }
+            }
+        }
     end
 
     return {
@@ -80,28 +123,6 @@ SMODS.current_mod.credits_tab = function()
             padding = 0.2,
             colour = G.C.BLACK
         },
-        nodes = {
-            --#region Bit with the localized text, can remove/change if unneccessary
-            {
-                n = G.UIT.R,
-                config = {
-                    align = "tm",
-                    padding = 0,
-                    minw = 8,
-                    minh = 3
-                },
-                nodes = {
-                    desc_from_rows(loc_nodes, true, 7)
-                }
-            },
-            --#endregion
-            {
-                n = G.UIT.R,
-                config = { align = "cm", padding = 0.07, no_fill = true },
-                nodes = {
-                    { n = G.UIT.O, config = { object = G.deg_credits_area } }
-                }
-            }
-        }
+        nodes = row_nodes
     }
 end
