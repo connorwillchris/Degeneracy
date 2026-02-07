d1dc99340b334fba3d7e49cd1bf2637dc3baafaf
cloudzXIII <100033104+cloudzXIII@users.noreply.github.com>
2026-02-07 14:41:01 +0000

    Report Card
    fixed it

--- Items/jokers/report_card.lua
+++ Items/jokers/report_card.lua
@@ -20,20 +20,21 @@ SMODS.Joker {
         }
     },
     calculate = function(self, card, context)
-        if context.end_of_round and context.game_over and context.main_eval then
+        if context.end_of_round and context.game_over == false and context.main_eval and not context.blueprint then
             local should_apply = false
             if G.GAME.dollars >= card.ability.extra.dollars then
+                ease_dollars(-card.ability.extra.dollars)
                 should_apply = true
             end
-            ease_dollars(-card.ability.extra.dollars)
             G.E_MANAGER:add_event(Event({
                 trigger = 'after',
                 delay = 0.7,
                 func = function()
-                    SMODS.calculate_context({ playing_card_added = true, cards = SMODS.add_card { set = "Base", rank = 'Ace', edition = (should_apply and "e_foil" or nil) } })
+                    SMODS.calculate_context({ playing_card_added = true, cards = SMODS.add_card { area = G.deck, set = "Base", rank = 'Ace', edition = (should_apply and "e_foil" or nil) } })
                     return true
                 end
             }))
+            SMODS.calculate_effect({ message = should_apply and "Foil Ace!" or "Ace!", colour = G.C.FILTER }, card)
         end
     end,
 }
