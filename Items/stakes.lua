9cee23c042ddcd2d12f07d638c472181bc2c8005
cloudzXIII <100033104+cloudzXIII@users.noreply.github.com>
2026-02-08 11:28:09 +0000

    Brass Stake rework
    Makes it so there's always 7 cards with a locked sticker in brass stake
    
    (Commented out) - At the end of the round, applies locked to a random card (could potentially be a rework for the stake or a deck)

--- Items/stakes.lua
+++ Items/stakes.lua
@@ -19,15 +19,30 @@ SMODS.Stake({
     calculate = function(self, context)
         if G.GAME.modifiers.enable_deg_locked and not G.GAME.brass_applied then
             G.GAME.brass_applied = true -- so it only does it once
-            for i = 1, #G.playing_cards do
-                if SMODS.pseudorandom_probability(self, "deg_seed", 1, 7, "brass") then
-                    SMODS.Stickers["deg_locked"]:apply(G.playing_cards[i], true)
+            local count = 0
+            while count < 7 do
+                for i = 1, #G.playing_cards do
+                    if SMODS.pseudorandom_probability(self, "deg_seed", 1, 7, "brass") and count < 7 then
+                        count = count + 1
+                        SMODS.Stickers["deg_locked"]:apply(G.playing_cards[i], true)
+                    end
                 end
             end
         end
+        --[[ At the end of the round, applies locked to a random card (Commented out for now)
+        if context.end_of_round and context.game_over == false and context.main_eval and not context.blueprint and context.beat_boss then
+            local unlocked = {}
+            for i = 1, #G.playing_cards do
+                if not G.playing_cards[i].ability.deg_locked then
+                    table.insert(unlocked, G.playing_cards[i])
+                end
+            end
+            local _card = pseudorandom_element(unlocked, "locked")
+            SMODS.Stickers["deg_locked"]:apply(_card, true)
+            SMODS.calculate_effect({ message = "Locked!" }, G.deck)
+        end--]]
     end,
     modifiers = function()
         G.GAME.modifiers.enable_deg_locked = true
     end,
 })
-
