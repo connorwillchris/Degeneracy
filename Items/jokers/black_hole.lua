d1721927fdf12ae9cfdf69ffb52c892f484c64c0
Gud <toby.hayter10@gmail.com>
2026-02-19 19:41:52 +0000

    Update black_hole.lua

--- Items/jokers/black_hole.lua
+++ Items/jokers/black_hole.lua
@@ -1,6 +1,5 @@
 SMODS.Joker {
-    key = "black_hole",
-
+    key = "notblackhole",
     loc_vars = function(self, info_queue, card)
         local numerator, denominator = SMODS.get_probability_vars(card, card.ability.extra.base, card.ability.extra.odds,
             'black_hole')
@@ -11,18 +10,21 @@ SMODS.Joker {
             }
         }
     end,
-
     rarity = 2,
     cost = 6,
-    --atlas = 'deg_jokers',
-    pos = { x = 0, y = 0 },
-
+    atlas = 'deg_jokers',
+    pos = { x = 9, y = 2 },
     config = {
         extra = {
             base = 1,
             odds = 3,
         }
     },
+    draw = function(self, card, layer)
+        if (layer == 'card' or layer == 'both') and card.sprite_facing == 'front' then
+            card.children.center:draw_shader('booster', nil, card.ARGS.send_to_shader)
+        end
+    end,
     calculate = function(self, card, context)
         if context.before and SMODS.pseudorandom_probability(card, 'black_hole', card.ability.extra.base, card.ability.extra.odds, 'deg_black_hole') then
             SMODS.calculate_effect({ message = localize("k_level_up_ex"), colour = G.C.FILTER }, card)
