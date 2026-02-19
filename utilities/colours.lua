9b29bd4dd3fb77560747b8321a9dfa0f049a53f4
Gud <toby.hayter10@gmail.com>
2026-02-19 19:44:06 +0000

    Add pride gradient colors to colours.lua

---
+++ utilities/colours.lua
@@ -0,0 +1,14 @@
+SMODS.Gradient {
+  key = "pride",
+  colours = {
+    HEX('fd5f55'),
+    HEX('fda200'),
+    HEX('f3c851'),
+    HEX('55a383'),
+    HEX('009cfd'),
+    HEX('8a71e1'),
+    HEX('fd7fa2'),
+  },
+  cycle = 3.5,
+  interpolation = 'trig',
+}
