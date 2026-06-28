be7e750847378290cd5af80c05f4934cfa8542e0
cloudzXIII <100033104+cloudzXIII@users.noreply.github.com>
2026-06-28 15:46:06 +0100

    e

--- utilities/atlases.lua
+++ utilities/atlases.lua
@@ -1,86 +1,80 @@
 -- Balatro Logo
 
 SMODS.Atlas({
-    key = "balatro",
-    path = "balatro.png",
-    px = 333,
-    py = 216,
-    prefix_config = { key = false },
+  key = "balatro",
+  path = "balatro.png",
+  px = 333,
+  py = 216,
+  prefix_config = { key = false },
 })
 
 -- Decks
 SMODS.Atlas {
-    key = "decks",
-    path = "decks.png",
-    px = 71,
-    py = 95
+  key = "decks",
+  path = "decks.png",
+  px = 71,
+  py = 95
 }
 
 -- Jokers
 SMODS.Atlas({
-    key = "deg_jokers",
-    path = "jokers.png",
-    px = 71,
-    py = 95,
+  key = "deg_jokers",
+  path = "jokers.png",
+  px = 71,
+  py = 95,
 })
 
 
 --#region Ranks
 SMODS.Atlas {
-    key = "cards_lc",
-    path = "ranks_lc.png",
-    px = 71,
-    py = 95
+  key = "cards_lc",
+  path = "ranks_lc.png",
+  px = 71,
+  py = 95
 }
 
 SMODS.Atlas {
-    key = "cards_hc",
-    path = "ranks_hc.png",
-    px = 71,
-    py = 95
+  key = "cards_hc",
+  path = "ranks_hc.png",
+  px = 71,
+  py = 95
 }
 
-SMODS.Atlas {
-    key = "deg_pure_ranks",
-    path = "pure_ranks.png",
-    px = 71,
-    py = 95
-}
 --#endregion
 
 SMODS.Atlas({
-    key = "deg_stakes",
-    path = "stakes.png",
-    px = 29,
-    py = 29,
+  key = "deg_stakes",
+  path = "stakes.png",
+  px = 29,
+  py = 29,
 })
 
 SMODS.Atlas({
-    key = "deg_stickers",
-    path = "Stickers.png",
-    px = 71,
-    py = 95,
+  key = "deg_stickers",
+  path = "Stickers.png",
+  px = 71,
+  py = 95,
 })
 
 SMODS.Atlas {
-    key = "credits",
-    path = "thanks-to-these-people.png",
-    px = 71,
-    py = 95
+  key = "credits",
+  path = "thanks-to-these-people.png",
+  px = 71,
+  py = 95
 }
 
 SMODS.Atlas {
-    key = "consumables",
-    path = "consumables.png",
-    px = 71,
-    py = 95
+  key = "consumables",
+  path = "consumables.png",
+  px = 71,
+  py = 95
 }
 
 SMODS.Atlas {
-    key = "deg_blinds",
-    path = "blinds.png",
-    px = 34,
-    py = 34,
-    atlas_table = 'ANIMATION_ATLAS',
-    frames = 21,
+  key = "deg_blinds",
+  path = "blinds.png",
+  px = 34,
+  py = 34,
+  atlas_table = 'ANIMATION_ATLAS',
+  frames = 21,
 }
